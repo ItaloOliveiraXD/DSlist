7d25daf613dae52b7b2d4c57c8dcc91c2ebaf2ca
ítalo.oliveira <italooliveira-xd@hotmail.com>
2023-07-11 08:38:10 -0300

    properties, Game, ORM

---
+++ src/main/java/br/com/projeto/dslist/entities/Game.java
@@ -0,0 +1,29 @@
+package br.com.projeto.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_games")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(of = "id")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+}
