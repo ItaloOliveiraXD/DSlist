b1810f50021fad328a979c1cbff30f9ef46c0a45
Italo.Oliveira <italooliveira-xd@hotmail.com>
2023-07-12 09:56:58 -0300

    Database seed update.

--- src/main/java/br/com/projeto/dslist/entities/BelongingPK.java
+++ src/main/java/br/com/projeto/dslist/entities/BelongingPK.java
@@ -17,6 +17,6 @@ public class BelongingPK {
     @JoinColumn(name = "game_id")
     private Game game;
     @ManyToOne
-    @JoinColumn(name = "gameList_id")
+    @JoinColumn(name = "list_id")
     private GameList gameList;
 }
